8206025f9cc546de4a20234e5322dd2d1631849c
Oleksii Filonenko <brightone@protonmail.com>
2019-11-08 11:16:51 +0200

    Refactor

--- home/default.nix
+++ home/default.nix
@@ -4,26 +4,23 @@
   imports = [ <home-manager/nixos> ];
   home-manager.useUserPackages = true;
 
-  # Disable ad-hoc user management.
-  users.mutableUsers = false;
-  users.defaultUserShell = pkgs.zsh;
+  users = {
+    # Disable ad-hoc user management.
+    mutableUsers = false;
+    defaultUserShell = pkgs.zsh;
 
-  users.users.brightone = {
-    isNormalUser = true;
-    extraGroups = [ "audio" "docker" "networkmanager" "video" "wheel" ];
-    hashedPassword = "$6$Hbwcp8inqhM0K$cuw2YW25pwTVSJcxOwRLuDszM1InXXeMSGXNLSXMAd7DWQayTUXwinXEOQZ1vydbafDqFHle0eVZPqAuMzRBw0";
+    users.brightone = {
+      isNormalUser = true;
+      extraGroups = [ "audio" "docker" "networkmanager" "video" "wheel" ];
+      hashedPassword =
+        "$6$Hbwcp8inqhM0K$cuw2YW25pwTVSJcxOwRLuDszM1InXXeMSGXNLSXMAd7DWQayTUXwinXEOQZ1vydbafDqFHle0eVZPqAuMzRBw0";
+    };
   };
 
   home-manager.users.brightone = { pkgs, ... }: {
     nixpkgs.config.allowUnfree = true;
 
-    imports = [
-      ./email.nix
-      ./files
-      ./packages.nix
-      ./programs
-      ./services
-      ./themes.nix
-    ];
+    imports =
+      [ ./email.nix ./files ./packages.nix ./programs ./services ./themes.nix ];
   };
 }
