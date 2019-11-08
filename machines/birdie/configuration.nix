d721cce8d829d397e47c3b79a580d2d0cd741db6
Oleksii Filonenko <brightone@protonmail.com>
2019-11-08 11:13:29 +0200

    Enable bluetooth on birdie

--- machines/birdie/configuration.nix
+++ machines/birdie/configuration.nix
@@ -1,31 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ 
-      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-      ../../configuration.nix
-      ../../profiles/efi.nix
-    ];
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ../../configuration.nix
+    ../../profiles/efi.nix
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-label/swap"; }
-    ];
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
@@ -34,6 +31,8 @@
 
   services.fprintd.enable = true;
   services.illum.enable = true;
+
+  hardware.bluetooth.enable = true;
 
   home-manager.users.brightone = { pkgs, ... }: {
     services.polybar.config."bar/main" = {
