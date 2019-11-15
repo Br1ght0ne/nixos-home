74d731c3f4c5a07f68624af26addaf2fef80f0f8
Oleksii Filonenko <brightone@protonmail.com>
2019-11-15 16:59:45 +0200

    Move to prezto with zpreztorc

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -1,45 +1,36 @@
 { pkgs, ... }:
 
 {
+  home.file.".zpreztorc".source = ./zpreztorc;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autocd = true;
-    plugins = with pkgs;
-      let
-        prezto = fetchFromGitHub {
+    initExtra = ''
+      source $HOME/.asdf/asdf.sh
+    '';
+    plugins = with pkgs; [
+      {
+        name = "prezto";
+        src = fetchFromGitHub {
           owner = "sorin-ionescu";
           repo = "prezto";
           rev = "2d6205e71404704eecc4f402e5b09d7cbd19bab7";
           sha256 = "1ks4vzgh033aw7xr10q1cy6ikmhwz2qbqiqb7056sb0y8kmh86wh";
           fetchSubmodules = true;
         };
-        preztoModule = module: {
-          name = "prezto-${module}";
-          src = "${prezto}/modules/${module}";
-          file = "init.zsh";
+        file = "init.zsh";
+      }
+      rec {
+        name = "fast-syntax-highlighting";
+        src = fetchFromGitHub {
+          owner = "zdharma";
+          repo = name;
+          rev = "d8e5bbe3dade87216d1a0dddb59a1c148b206e24";
+          sha256 = "1ks4vzgh033aw7xr10q1cy6ikmhwz2qbqiqb7056sb0y8kmh86wh";
         };
-      in [
-        {
-          name = "prezto";
-          src = prezto;
-          file = "init.zsh";
-        }
-        rec {
-          name = "fast-syntax-highlighting";
-          src = fetchFromGitHub {
-            owner = "zdharma";
-            repo = name;
-            rev = "d8e5bbe3dade87216d1a0dddb59a1c148b206e24";
-            sha256 = "1ks4vzgh033aw7xr10q1cy6ikmhwz2qbqiqb7056sb0y8kmh86wh";
-          };
-        }
-      ] ++ map preztoModule [
-        "utility"
-        "completion"
-        "history-substring-search"
-        "docker"
-        "git"
-      ];
+      }
+    ];
   };
 }
