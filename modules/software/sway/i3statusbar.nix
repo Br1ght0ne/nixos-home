521fe06c767356a08ec76324ae2c4fc4825cdc96
Oleksii Filonenko <brightone@protonmail.com>
2019-11-25 21:17:41 +0200

    Fix i3status-rust package name

--- modules/software/sway/i3statusbar.nix
+++ modules/software/sway/i3statusbar.nix
@@ -4,5 +4,5 @@
   home-manager.users.brightone = {
     xdg.configFile."i3statusbar.toml".source = ./.config/i3statusbar.toml;
   };
-  programs.sway.extraPackages = with pkgs; [ i3status-rs ];
+  programs.sway.extraPackages = with pkgs; [ i3status-rust ];
 }
